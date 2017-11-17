a76e1d7b8bf8c7d88e3a391d0a765abd9dcf48e2
tymokvo <tymokvo@gmail.com>
2017-11-17 10:11:32 -0800

    initial commit

---
+++ mathnetwrapper.py
@@ -0,0 +1,83 @@
+"""
+A wrapper for MathNet.Numerics
+https://numerics.mathdotnet.com/api/
+
+
+Full MathNet.Numerics namespace available as MN
+
+Example:
+
+import mathnetwrapper as mnw
+
+x = mnw.matrix((2, 2), distribution=mnw.MN.Distributions.Normal())
+
+print x
+DenseMatrix 2x2-Double
+ 0.308277 -0.47732
+-0.0486687 1.38952
+
+More to come...
+
+"""
+
+from System import Environment
+from clr import AddReferenceToFileAndPath as addref
+mathnet_path = "PATH TO MATHNET FOLDER"
+path = "{}\\MathNet.Numerics\\lib\\net40\\MathNet.Numerics.dll".format(mathnet_path)
+addref(path)
+import MathNet.Numerics as MN
+from System import Array
+
+
+### .NET to Python aliases
+mnDoubleVector = MN.LinearAlgebra.Double.Vector.Build.DenseOfArray
+mnDoubleMatrix = MN.LinearAlgebra.Double.DenseMatrix
+
+
+### Wrapper functions TODO: just inherit from the .NET class
+def vector(input_list):
+    """
+    Turn a Python List into a vector of Doubles
+
+    x = [1, 2, 3, 4] # a python list
+
+    x = vector(x)
+
+    print x
+    DenseVector 4 Double
+    1
+    2
+    3
+    4
+    """
+    arr = Array[float](input_list)
+    return mnDoubleVector(arr)
+
+
+def matrix((rows, columns), input_list=None, fill_value=None, distribution=None):
+    """
+    Turn a Python List into a matrix of Doubles (Column-major order)
+
+    x = [1, 2, 3, 4] # a python list
+
+    x = matrix(x)
+
+    print x
+    DenseMatrix 2x2-Double
+    1 3
+    2 4
+    """
+
+    if input_list != None:
+        assert(len(input_list) == rows * columns, "Matrix rank must equal input list length")
+        arr = Array[float](input_list)
+        matrix = mnDoubleMatrix(rows, columns, arr)
+        return matrix
+
+    if fill_value != None:
+        matrix = mnDoubleMatrix.Create(rows, columns, fill_value)
+        return matrix
+
+    if distribution != None:
+        matrix = mnDoubleMatrix.CreateRandom(rows, columns, distribution)
+        return matrix
