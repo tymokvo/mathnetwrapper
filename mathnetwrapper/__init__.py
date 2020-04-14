0ae8588dcbb1612eb60950cde8aa6600cfa43aa9
Tyler Kvochick <tymokvo@gmail.com>
2020-04-14 07:54:20 -0700

    Add module stuff

---
+++ mathnetwrapper/__init__.py
@@ -0,0 +1,81 @@
+from System import Array
+from clr import AddReferenceToFileAndPath as addref
+PATH = r"C:\Users\tyler\.nuget\packages\mathnet.numerics\4.9.1\lib\net40\MathNet.Numerics.dll"
+addref(PATH)
+import MathNet.Numerics as MN
+import itertools
+
+mnDoubleVector = MN.LinearAlgebra.Double.Vector.Build.DenseOfArray
+mnDoubleMatrix = MN.LinearAlgebra.Double.DenseMatrix
+
+def list_transpose_stack(input_list):
+    """
+    Transpose row-major python list of lists into column-major for MathNet matrix
+
+    a = [1, 2, 3, 4]
+    b = [a, a[::-1]]
+
+    c = mnw.matrix((2, 4), input_list=mnw.list_transpose_stack(b))
+
+    print b, c
+    [[1, 2, 3, 4], [4, 3, 2, 1]]
+    DenseMatrix 2x4-Double
+    1 2 3 4
+    4 3 2 1
+    """
+
+    out = map(list, zip(*input_list))
+
+    out = itertools.chain(*out)
+
+    return [i for i in out]
+
+
+def vector(input_list):
+    """
+    Turn a Python List into a vector of Doubles
+
+    x = [1, 2, 3, 4] # a python list
+
+    x = vector(x)
+
+    print x
+    DenseVector 4 Double
+    1
+    2
+    3
+    4
+    """
+    arr = Array[float](input_list)
+    return mnDoubleVector(arr)
+
+
+def matrix((rows, columns), input_list=None, transpose=False, fill_value=None, distribution=None):
+    """
+    Turn a Python List into a matrix of Doubles (Column-major order)
+
+    x = [1, 2, 3, 4] # a python list
+
+    x = matrix(x)
+
+    print x
+    DenseMatrix 2x2-Double
+    1 3
+    2 4
+    """
+
+    if input_list != None:
+        if transpose:
+            input_list = list_transpose_stack(input_list)
+        assert(len(input_list) == rows * columns, "Matrix rank must equal input list length")
+        arr = Array[float](input_list)
+        matrix = mnDoubleMatrix(rows, columns, arr)
+        return matrix
+
+    if fill_value != None:
+        matrix = mnDoubleMatrix.Create(rows, columns, fill_value)
+        return matrix
+
+    if distribution != None:
+        matrix = mnDoubleMatrix.CreateRandom(rows, columns, distribution)
+        return matrix
